c47a35a9822bcc4aaa31c86b8e28891b339d58a3
dom-m17 <dominicmaynard00@gmail.com>
2024-05-06 19:46:57 +0100

    Changing to SQL

--- task/read/displayAllTasks.go
+++ task/read/displayAllTasks.go
@@ -1,25 +1,57 @@
 package read
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
-	"gopracticeproject/resources"
+	"os"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func DisplayAllTasks() {
-	exampleTasks := resources.ExampleTasks
+	fmt.Println()
 	fmt.Println("Here are all of your tasks:")
-	anyTaskFound := false
-	for _, task := range exampleTasks {
-		anyTaskFound = true
-		fmt.Printf("ID: %d\n", task.ID)
-		fmt.Printf("Title: %s\n", task.Title)
-		fmt.Printf("Description: %s\n", task.Description)
-		fmt.Printf("Deadline: %s\n", task.Deadline)
-		fmt.Printf("Status: %s\n", task.Status)
+	dbpool, err := pgxpool.New(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to create connection pool: %v\n", err)
+		os.Exit(1)
+	}
+	defer dbpool.Close()
+
+	rows, err := dbpool.Query(context.Background(), "select * from tasks;")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	for rows.Next() {
+		var id int
+		var title string
+		var description sql.NullString
+		var deadline time.Time
+		var status string
+
+		err := rows.Scan(&id, &title, &description, &deadline, &status)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Scan failed: %v\n", err)
+			os.Exit(1)
+		}
+
+		var descString string
+		if description.Valid {
+			descString = description.String
+		} else {
+			descString = "NULL"
+		}
+
+		fmt.Printf("Title: %s\n", title)
+		fmt.Printf("Description: %s\n", descString)
+		fmt.Printf("Deadline: %s\n", deadline.Format("02-01-2006"))
+		fmt.Printf("Status: %s\n", status)
 		fmt.Println()
 	}
 
-	if !anyTaskFound {
-		fmt.Println("No tasks found")
-	}
+	fmt.Println("End of tasks")
 }
